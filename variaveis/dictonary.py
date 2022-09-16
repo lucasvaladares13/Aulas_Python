0a02bc1fb4c6889e51a5212f8a7826da728e5c9f
lucasvaladares13 <lucasvao96@gmail.com>
2022-09-16 09:48:28 -0300

    First Commit

---
+++ variaveis/dictonary.py
@@ -0,0 +1,17 @@
+var_dict_aluno = {"aluno1":"Gabriel",
+            "aluno2":"Lucas",
+            "aluno3":"Freitinhas",
+            "aluno4":"Miguelzim",
+            "aluno5":"Duduzim",
+            "aluno6":"Panqueca",
+    
+            }
+
+var_dict_nota = {"aluno1":10,
+            "aluno2":10,
+            "aluno3":9,
+            "aluno4":5,
+            "aluno5":6,
+            "aluno6":8,
+    
+            }
